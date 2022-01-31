e8889a7f1e7eacfc1548fb8969f24adf3c931c5b
tjdans34568 <tjdans34568@gmail.com>
2022-01-31 15:23:24 +0900

    2 commit

---
+++ 메테오/02.typeof_test/typeof_test.js
@@ -0,0 +1,6 @@
+console.log("type? : " + typeof "Meteor");
+console.log("type? : " + typeof 29);
+console.log("type? : " + typeof true);
+console.log("type? : " + typeof { x: 1, y: 2 });
+console.log("type? : " + typeof undefined);
+console.log("type? : " + typeof null);
