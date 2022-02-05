9b332c65665a8c8f8c0832c179fdd3f89bc62818
tjdans34568 <tjdans34568@gmail.com>
2022-02-05 15:22:10 +0900

    ..

---
+++ 메테오/06.조건연산자/testl.js
@@ -0,0 +1,26 @@
+// 조건문 vs 반복문 vs 함수
+// if vs while
+// 조건식
+if (person == "배고파") {
+  console.log("밥 먹는다.");
+}
+
+// if vs switch
+
+// 반복문
+
+// for vs while vs do while
+
+// for : 이것을 10번 드세요
+
+// while : 배부를 때 까지 드세요
+
+// do while : 무조건 한번은 드세요
+
+for (let i = 0; i < 10; i++) {
+  if (i % 2 === 0) {
+    console.log("짝수입니다.");
+  }
+}
+
+if (age >= 20) alert("성인입니다. 입장하세요.");
