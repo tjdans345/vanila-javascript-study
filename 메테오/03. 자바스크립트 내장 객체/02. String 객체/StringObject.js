4dcf2fecf8ffc631e865185aa1e3ecf41a07329e
tjdans34568 <tjdans34568@gmail.com>
2022-02-09 23:27:15 +0900

    String Object

--- 메테오/03. 자바스크립트 내장 객체/02. String 객체/StringObject.js
+++ 메테오/03. 자바스크립트 내장 객체/02. String 객체/StringObject.js
@@ -35,3 +35,32 @@ console.log(-fristPosition2);
 console.log(-secondPosition2);
 
 console.log(text2.slice(-fristPosition2, -secondPosition2));
+
+// substr() 응용 버젼 !
+let 변수명뭐하지 = "나는 문어 꿈을꾸는문어 입니다.";
+let 찾는문자열인덱스 = 변수명뭐하지.indexOf("꿈");
+let 뽑아낼문자길이 = 변수명뭐하지.slice(찾는문자열인덱스).length;
+
+console.log(변수명뭐하지);
+console.log(찾는문자열인덱스);
+console.log(뽑아낼문자길이);
+
+let 돌고돌아드디어찾았음코드줄여봐야지 = 변수명뭐하지.substr(
+  찾는문자열인덱스,
+  뽑아낼문자길이
+);
+console.log(돌고돌아드디어찾았음코드줄여봐야지);
+
+// replace() 응용
+let 변수명 = "안녕 곤방와 싸와디깝ㅋㅋ";
+let 바꿔보자 = 변수명.replace("안녕", "헬로");
+console.log(바꿔보자);
+let 바꿔바꿔모든걸다바꿔 = "안녕 곤방와 싸와디깝 케케켘";
+// 응용해서 모든 공백 제거해버리기
+while (바꿔바꿔모든걸다바꿔.indexOf(" ") !== -1) {
+  바꿔바꿔모든걸다바꿔 = 바꿔바꿔모든걸다바꿔.replace(" ", "");
+}
+console.log(바꿔바꿔모든걸다바꿔);
+
+// 정규식을 이용하여 모든 공백 제거
+console.log(바꿔바꿔모든걸다바꿔.replace(/" "/g, ""));
