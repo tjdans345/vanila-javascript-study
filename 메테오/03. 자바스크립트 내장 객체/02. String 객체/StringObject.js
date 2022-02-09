62c9a771bbe01a0e9e19ec64c5345cff31c2d473
tjdans34568 <tjdans34568@gmail.com>
2022-02-09 22:03:43 +0900

    String Object

--- 메테오/03. 자바스크립트 내장 객체/02. String 객체/StringObject.js
+++ 메테오/03. 자바스크립트 내장 객체/02. String 객체/StringObject.js
@@ -1,2 +1,19 @@
+// length
 let test = "가나다라마바사아자차카타파하";
 let testLength = test.length;
+
+console.log(testLength);
+
+// indexOf()
+let str = "나는 메테오 입니다.";
+let position = str.indexOf("메테오");
+let position2 = str.indexOf("다");
+
+console.log(position);
+console.log(position2);
+
+// lastIndexOf()
+let str2 = "나는 메테오 입니다. 좋아하는 곡은 창모의 메테오 입니다.";
+let position3 = str2.lastIndexOf("메테오");
+
+console.log(position3);
