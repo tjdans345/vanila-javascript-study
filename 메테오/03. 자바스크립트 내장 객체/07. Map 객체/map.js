df0f416a73b9aea8c2d015be2e307d094cb39998
tjdans34568 <tjdans34568@gmail.com>
2022-02-14 23:11:48 +0900

    Map Object::

--- 메테오/03. 자바스크립트 내장 객체/07. Map 객체/map.js
+++ 메테오/03. 자바스크립트 내장 객체/07. Map 객체/map.js
@@ -12,3 +12,29 @@ userMap.set("슬롬", "박재빈");
 console.log(userMap.size);
 
 console.log(userMap.get("메테오"));
+console.log(userMap.has("메테오"));
+
+userMap.delete("메테오");
+console.log(userMap);
+
+// Map(7) {'션' => '주시온', '안젤라' => '정수진', '니키' => '이슬기', '윌리' => '왕현경', '웰람슨' => '백수원', …}
+// [[Entries]]
+// 0: {"션" => "주시온"}
+// 1: {"안젤라" => "정수진"}
+// 2: {"니키" => "이슬기"}
+// 3: {"윌리" => "왕현경"}
+// 4: {"웰람슨" => "백수원"}
+// 5: {"아가트" => "김보라"}
+// 6: {"슬롬" => "박재빈"}
+// size: 7
+// [[Prototype]]: Map=
+
+userMap.forEach((user) => console.log(user));
+
+// 주시온
+// 정수진
+// 이슬기
+// 왕현경
+// 백수원
+// 김보라
+// 박재빈
