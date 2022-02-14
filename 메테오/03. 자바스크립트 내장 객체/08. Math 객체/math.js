625f3bd423af8862a8957f858c83d75aaf03a76b
tjdans34568 <tjdans34568@gmail.com>
2022-02-14 23:19:39 +0900

    Math Object

---
+++ 메테오/03. 자바스크립트 내장 객체/08. Math 객체/math.js
@@ -0,0 +1,31 @@
+console.log(Math.round(4.5));
+console.log(Math.round(3));
+console.log(Math.round(2.7));
+console.log(Math.round(3.2));
+console.log(Math.round(5.0));
+
+// 5
+// 3
+// 3
+// 3
+// 5
+console.log("-------Math.ceil()-------");
+console.log(Math.ceil(4.5));
+console.log(Math.ceil(3));
+console.log(Math.ceil(2.7));
+console.log(Math.ceil(3.2));
+console.log(Math.ceil(5.0));
+
+// -------Math.ceil()-------
+// 5
+// 3
+// 3
+// 4
+// 5
+
+console.log("-------Math.floor()-------");
+console.log(Math.floor(4.5));
+console.log(Math.floor(3));
+console.log(Math.floor(2.7));
+console.log(Math.floor(3.2));
+console.log(Math.floor(5.0));
