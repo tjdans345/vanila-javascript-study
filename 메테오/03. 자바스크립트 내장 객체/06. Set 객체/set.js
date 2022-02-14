945b0b717a4cf8c9dfb8641f3176f001af4b3e69
tjdans34568 <tjdans34568@gmail.com>
2022-02-14 22:53:42 +0900

    Map Object

--- 메테오/03. 자바스크립트 내장 객체/06. Set 객체/set.js
+++ 메테오/03. 자바스크립트 내장 객체/06. Set 객체/set.js
@@ -10,3 +10,17 @@ console.log(mySet);
 
 console.log(mySet.has("호미들"));
 console.log(mySet.has("창모"));
+
+mySet.add("호미들");
+console.log(mySet);
+mySet.delete("호미들");
+console.log(mySet);
+// mySet.clear();
+// console.log(mySet);
+
+mySet.forEach((item, item2, mySet) => console.log(mySet.has("창모")));
+
+// 창모
+// 구창모
+// 덕소 쉐키
+// 덕소의 아들
---
+++ 메테오/03. 자바스크립트 내장 객체/07. Map 객체/map.js
@@ -0,0 +1,12 @@
+let userMap = new Map();
+
+userMap.set("메테오", "엄성문");
+userMap.set("션", "주시온");
+userMap.set("안젤라", "정수진");
+userMap.set("니키", "이슬기");
+userMap.set("윌리", "왕현경");
+userMap.set("웰람슨", "백수원");
+userMap.set("아가트", "김보라");
+userMap.set("슬롬", "박재빈");
+
+console.log(userMap.size);
