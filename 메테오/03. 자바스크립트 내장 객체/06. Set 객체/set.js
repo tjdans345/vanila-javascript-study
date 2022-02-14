bd74759fa2210782bf6f537242966e2942b9889b
tjdans34568 <tjdans34568@gmail.com>
2022-02-14 22:33:22 +0900

    Set Object

---
+++ 메테오/03. 자바스크립트 내장 객체/06. Set 객체/set.js
@@ -0,0 +1,12 @@
+let mySet = new Set();
+
+mySet.add("창모");
+mySet.add("구창모");
+mySet.add("덕소 쉐키");
+mySet.add("덕소의 아들");
+mySet.add("덕소의 아들");
+
+console.log(mySet);
+
+console.log(mySet.has("호미들"));
+console.log(mySet.has("창모"));
