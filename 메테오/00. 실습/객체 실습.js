c3fd508c4351c76d7a327d8c94393108063a35fc
tjdans345 <tjdans34568@gmail.com>
2022-02-26 09:58:39 +0900

    ..

--- 메테오/00. 실습/객체 실습.js
+++ 메테오/00. 실습/객체 실습.js
@@ -53,3 +53,38 @@ console.log(object["+"]);
 console.log(object["-"]);
 console.log(object["*"]);
 console.log(object["/"]);
+
+// 자기 자신을 자바스크립트 객체로 표현하라
+let meteor = {
+  name: "성문",
+  age: 29,
+  homeGround: "Busan",
+  say: function () {
+    console.log("메테오 메테오 거대 메테오~~!");
+  },
+};
+
+console.log(meteor.name);
+
+function yupDoYouHave(arr, value) {
+  if (arr.indexOf(value) != -1) {
+    console.log(`I have ${value} in my ${arr.indexOf(value)} pocket`);
+  } else {
+    console.log("I don't have it");
+  }
+}
+
+function yupDoYouHave2(arr, value) {
+  arr.find((myArr, i) => {
+    if (myArr == value) {
+      console.log(`I have ${value} in my ${i} pocket`);
+    } else {
+      console.log("I don't have it");
+    }
+  });
+}
+
+var arr = ["Hej", 2000, true, 250000];
+
+yupDoYouHave(arr, 2000);
+yupDoYouHave2(arr, "Hello");
---
+++ 메테오/00. 실습/객체 실습2.js
@@ -0,0 +1,32 @@
+// 배열 요소의 값을 받으면 그 요소를 배열에서 찾아서 인덱스를 반환하는 함수find(arr, value)를 작성해보자.
+let array = [
+  "안젤라",
+  "아가트",
+  "니키",
+  "윌리",
+  "웰람슨",
+  "션",
+  "슬롬",
+  "메테오",
+];
+
+function findArray(arr, value) {
+  let find = 0; // 배열안에 찾는 값이 존재하는지 확인하려고 임의로 만든 필드
+
+  // for문으로 바꾼거 올려놓기
+
+    arr.forEach((element, index) => {
+      if (element === value) {
+
+        console.log(`배열 요소의 값은 ${index} 번째 인덱스에 위치하고 있습니다.`);
+
+        find = 1; // 배열안에 찾는 값이 존재하는지 확인하려고 임의로 만든 필드
+      }
+    });
+
+  if (find === 0) {
+    console.log("찾으시는 값이 배열안에 존재하지 않습니다.");
+  }
+}
+
+findArray(array, "zz");
