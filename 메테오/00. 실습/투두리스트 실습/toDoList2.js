77c29b99caa0014bfb63900733c6589fe432b51c
tjdans345 <tjdans34568@gmail.com>
2022-02-27 17:55:27 +0900

    ..

--- 메테오/00. 실습/투두리스트 실습/toDoList2.js
+++ 메테오/00. 실습/투두리스트 실습/toDoList2.js
@@ -1,13 +1,19 @@
+/**
+ * 투두리스트 Ver1.  Developer Meteor
+ * 이번 TODO 리스트 구현 목표
+ * 1. let, const 변수 활용하기
+ * 2. 일반 function 보다 애로우 펑션을 활용할 것
+ * 3. 자바스크립트 스터디에서 배운 개념에서 최대한 구현할 것
+ */
+
 // 현재시간 구하는 함수 Date객체 이용
 const timerFunction = () => {
   const timer = document.getElementById("timer");
-
   const today = new Date();
   let hour = today.getHours();
   if (hour == 0) {
     hour = 12;
   }
-
   hour > 12 ? (hour = `PM ${hour - 12}`) : `AM ${hour}`;
   timer.innerHTML = `현재 시간 ${addZero(hour)} : ${addZero(
     today.getMinutes()
@@ -19,7 +25,9 @@ const addZero = (num) => {
   return num < 10 ? (num = `0${num}`) : `${num}`;
 };
 
-// 시간 뿌려주는 함수
+/**
+ * 초기 시간 작동 및 1초마다 시간 리셋
+ */
 const timeStart = () => {
   timerFunction();
   setInterval(timerFunction, 1000);
@@ -27,9 +35,30 @@ const timeStart = () => {
 
 window.onload = timeStart();
 
-// 투두 추가 버튼 클릭 이벤트 함수
-const todoAdd = function () {
-  const todoList = document.getElementById("todo-list");
+/**
+ * 투두리스트 추가 버튼 이벤트
+ */
+const todoAdd = () => {
   let todoContent = document.getElementById("todo-content").value;
-  todoList.append(todoContent);
+  const tasks = document.getElementById("tasks");
+  tasks.append(makeHtml(todoContent));
+};
+
+/**
+ * 투두리스트 동적 요소 만들기
+ * @param {*} content
+ * @returns 추가되는 투두 컨텐츠
+ */
+const makeHtml = (content) => {
+  const todoContent = document.createElement("div");
+  todoContent.className = "task";
+  todoContent.innerHTML = `
+  <span id="taskname"> ${content}</span>
+  <button class="delete" id="delete-btn" onClick="deleteBtn(this)">삭제</button>`;
+  return todoContent;
+};
+
+const deleteBtn = (target) => {
+  const taskNode = target.parentNode;
+  taskNode.parentNode.removeChild(taskNode);
 };
