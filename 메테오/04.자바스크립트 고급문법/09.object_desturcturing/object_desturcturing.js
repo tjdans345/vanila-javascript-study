ec633e52da5c148be4085a239caa207ded608aee
tjdans345 <tjdans34568@gmail.com>
2022-03-05 12:48:51 +0900

    array_destructuring

---
+++ 메테오/04.자바스크립트 고급문법/09.object_desturcturing/object_desturcturing.js
@@ -0,0 +1,20 @@
+const getObject = () => {
+  return {
+    username: "메테오",
+    age: 29,
+    emial: "tjdans34568@gmail.com",
+  };
+};
+
+// 이렇게 Object의 필요한 키 값만 받아서 사용 가능하다!! 와 신기방기하네요
+let { username, age } = getObject();
+
+console.log(username);
+console.log(age);
+
+// 함수 파라미터에서도 적용 가능!
+function test({ username, age }) {
+  console.log(`${username}는 ${age}살 이지롱`);
+}
+
+test(getObject());
