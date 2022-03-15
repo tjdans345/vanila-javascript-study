2c99c0ee6aa60a97da77bb9868a4d2661af8731a
tjdans34568 <tjdans34568@gmail.com>
2022-03-16 00:38:32 +0900

    Error Object Study

--- 메테오/04.자바스크립트 고급문법/15.error/error.js
+++ 메테오/04.자바스크립트 고급문법/15.error/error.js
@@ -18,3 +18,26 @@ const test = (param) => {
     console.log(error.message);
   }
 };
+
+const foo = () => {
+  throw Error("foo에서 발생한 에러"); // new 키워드 생략 가능
+};
+
+const bar = () => {
+  foo();
+};
+
+const baz = () => {
+  bar();
+};
+
+try {
+  baz();
+} catch (error) {
+  console.log(error);
+}
+// Error: foo에서 발생한 에러
+//     at foo (error.js:23:9)
+//     at bar (error.js:27:3)
+//     at baz (error.js:31:3)
+//     at error.js:35:3
